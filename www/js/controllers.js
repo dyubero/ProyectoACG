a584f090ca250fad1c3dfbc632ba1b2dc8d29ae0
dyubero <dyubero96@gmail.com>
2016-09-27 13:05:44 +0200

    Primer commit en proceso singin y singup (falta meter conexion servidor)

---
+++ www/js/controllers.js
@@ -0,0 +1,54 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout,  $location, $ionicPopup) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  //--------------------------------------------
+   $scope.login = function(user) {
+			
+		if(typeof(user)=='undefined'){
+			$scope.showAlert('Por favor, rellene nombre de usuario y contraseña para continuar.');	
+			return false;
+		}
+
+		if(user.username=='Prueba@gmail.com' && user.password=='demo'){
+			$location.path('/app/dashboard');
+		}else{
+			$scope.showAlert('CONTRASEÑA O USUARIO INVALIDO.');	
+		}
+		
+	};
+  //--------------------------------------------
+  $scope.logout = function() {   $location.path('/app/login');   };
+  //--------------------------------------------
+   // An alert dialog
+	 $scope.showAlert = function(msg) {
+	   var alertPopup = $ionicPopup.alert({
+		 title: 'Mensaje de aviso',
+		 template: msg
+	   });
+	 };
+  //--------------------------------------------
+})
+
+.controller('ProfilesCtrl', function($scope , Profiles) {
+    $scope.profiles = Profiles.all();
+})
+
+.controller('ProfileCtrl', function($scope, $stateParams , Profiles) {
+	$scope.profile = Profiles.get($stateParams.profileId);
+})
+
+.controller('DashCtrl', function($scope, $stateParams , Profiles) {
+	$scope.profiles = Profiles.all();
+});
+
---
+++ www/js/services.js
@@ -0,0 +1,45 @@
+angular.module('starter.services', [])
+
+.factory('Profiles', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var profiles = [{
+    id: 0,
+    name: 'Coca-cola',
+    deseg: 'Lider en bebidas espirituosas',
+    face: 'img/150x165/Coca-cola.jpg'
+  }, {
+    id: 1,
+    name: 'Apple',
+    deseg: 'Inspirado en Bill Gates',
+    face: 'img/150x165/Apple.jpg'
+  }, {
+    id: 2,
+	name: 'Nike',
+	deseg: 'Just Do It ',
+    face: 'img/150x165/Nike.jpg'
+  }, {
+    id: 5,
+	name: 'Adidas',
+	deseg: 'All Day I Dream About Sport”',
+    face: 'img/150x165/Adidas.jpg'
+  }];
+
+  return {
+    all: function() {
+      return profiles;
+    },
+    remove: function(id) {
+      profiles.splice(profiles.indexOf(id), 1);
+    },
+    get: function(profileId) {
+      for (var i = 0; i < profiles.length; i++) {
+        if (profiles[i].id === parseInt(profileId)) {
+          return profiles[i];
+        }
+      }
+      return null;
+    }
+  };
+});
